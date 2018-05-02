fed4c16a17dd8de7d35bfadcb5d43191f087de88
Luke <lukemichaelvocalist@gmail.com>
2018-05-02 12:32:38 +0100

    Move schema to file in src

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 const { GraphQLServer } = require("graphql-yoga");
 
 //define GraphQL schema
-const typeDefs = `type Query {info:String!}`;
+const typeDefs = `
+type Query {
+  info:String!, 
+  feed:[Link!]!
+} 
+type Link {
+  id:ID!, 
+  description:String!, 
+  url:String!
+}
+type Mutation {
+  post(url: String!, description: String!): Link!
+}
+`;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
 
 //implementation of GraphQl schema
 const resolvers = {
   Query: {
-    info: () => "This is the API of a HackerNews clone"
+    info: () => "This is the API of a HackerNews clone",
+    feed: () => links
+  },
+  Link: {
+    id: root => root.id,
+    description: root => root.description,
+    url: root => root.url
   }
 };
 
