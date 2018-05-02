aad0bf8b4c28afdf2ef2bf6cf0cc29978226c5db
Luke <lukemichaelvocalist@gmail.com>
2018-05-02 12:37:54 +0100

    add mutation and remove schema from index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-//define GraphQL schema
-const typeDefs = `
-type Query {
-  info:String!, 
-  feed:[Link!]!
-} 
-type Link {
-  id:ID!, 
-  description:String!, 
-  url:String!
-}
-type Mutation {
-  post(url: String!, description: String!): Link!
-}
-`;
-
 let links = [
   {
     id: "link-0",
@@ -23,22 +7,27 @@ let links = [
     description: "Fullstack tutorial for GraphQL"
   }
 ];
-
+let idCount = links.length;
 //implementation of GraphQl schema
 const resolvers = {
   Query: {
     info: () => "This is the API of a HackerNews clone",
     feed: () => links
   },
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
-  }
+  Mutation: {
+    post: (root, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
