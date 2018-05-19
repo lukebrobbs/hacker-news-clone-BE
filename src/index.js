f31960cd1cab6dbc780e3cc73a717f122bdbe09a
lukebrobbs <lukebrobbin@Lukes-MacBook-Pro.local>
2018-05-19 12:29:24 +0100

    implement additional mutations and queries, along with resolvers

--- src/index.js
+++ src/index.js
@@ -12,18 +12,36 @@ let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => "This is the API of a HackerNews clone",
-    feed: () => links
+    feed: () => links,
+    link: (root, args) => links.filter(link => link.id === args.id)[0]
   },
   Mutation: {
     post: (root, args) => {
-       const link = {
+      const link = {
         id: `link-${idCount++}`,
         description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (root, args) => {
+      const link = {
+        id: args.id,
+        description: args.description,
+        url: args.url
+      };
+      const index = links.map(link => link.id).indexOf(args.id);
+      links[index] = link;
+      return link;
+    },
+    deleteLink: (root, args) => {
+      const index = links.map(link => link.id).indexOf(args.id);
+      const link = links[index];
+      links.splice(index, 1);
+      return link;
     }
+  }
 };
 
 const server = new GraphQLServer({
