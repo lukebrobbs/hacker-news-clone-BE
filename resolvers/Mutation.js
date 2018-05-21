3b43e72916cafe48e042d270e0cf106c241a7284
lukebrobbs <lukebrobbin@Lukes-MacBook-Pro.local>
2018-05-21 10:53:18 +0100

    work on auth

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET, getUserId } = require("../utils");
+
 async function signup(parent, args, context, info) {
   // 1
   const password = await bcrypt.hash(args.password, 10);
@@ -42,6 +46,20 @@ async function login(parent, args, context, info) {
     token,
     user
   };
+}
+
+function post(parent, args, context, info) {
+  const userId = getUserId(context);
+  return context.db.mutation.createLink(
+    {
+      data: {
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } }
+      }
+    },
+    info
+  );
 }
 
 module.exports = {
