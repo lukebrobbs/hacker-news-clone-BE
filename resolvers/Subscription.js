e2a502a2d5782085a4bf77164220b1c59eb4c3fc
lukebrobbs <lukebrobbin@Lukes-MacBook-Pro.local>
2018-05-21 12:25:46 +0100

    complete subscriptions, work on filters

--- resolvers/Subscription.js
+++ resolvers/Subscription.js
@@ -5,10 +5,22 @@ function newLinkSubscribe(parent, args, context, info) {
   );
 }
 
-const newLink = {
+const newlink = {
   subscribe: newLinkSubscribe
 };
 
+function newVoteSubscribe(parent, args, context, info) {
+  return context.db.subscription.vote(
+    { where: { mutation_in: ["CREATED"] } },
+    info
+  );
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe
+};
+
 module.exports = {
-  newLink
+  newlink,
+  newVote
 };
