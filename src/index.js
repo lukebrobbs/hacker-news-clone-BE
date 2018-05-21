95fc2758e12130831aba5677fa9fdf59e4909d85
lukebrobbs <lukebrobbin@Lukes-MacBook-Pro.local>
2018-05-21 12:45:20 +0100

    add sort and count

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const Query = require("../resolvers/Query");
 const Mutation = require("../resolvers/Mutation");
 const AuthPayload = require("../resolvers/AuthPayload");
 const Subscription = require("../resolvers/Subscription");
+const Feed = require("../resolvers/Feed");
 
 //implementation of GraphQl schema
 const resolvers = {
   Query,
   Mutation,
   AuthPayload,
-  Subscription
+  Subscription,
+  Feed
 };
 
 const server = new GraphQLServer({
